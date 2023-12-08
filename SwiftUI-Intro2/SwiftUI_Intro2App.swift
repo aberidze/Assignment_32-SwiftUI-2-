83be9ee52ed8369f072253b56674b59cf8282dbd
AnriBeridzee <aberidze02@gmail.com>
2023-12-09 03:33:04 +0400

    Initial Commit

---
+++ SwiftUI-Intro2/SwiftUI_Intro2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Intro2App.swift
+//  SwiftUI-Intro2
+//
+//  Created by Macbook Air 13 on 09.12.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Intro2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
